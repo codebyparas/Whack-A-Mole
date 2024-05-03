e61ed718b936e70250dd53d964868026a3323e46
Paras-Sachdeva <paras140902@gmail.com>
2024-05-03 22:02:25 +0530

    ADD: Pipes

--- mole.js
+++ mole.js
@@ -1,3 +1,12 @@
 window.onload = function(){
     setGame();
 }
+
+function setGame(){
+    // Set up the Grid for the Game Board in HTML
+    for(let i=0;i<9;i++){
+        let title=document.createElement("div");
+        title.id=i.toString();
+        document.getElementById("board").appendChild(title);
+    }
+}
